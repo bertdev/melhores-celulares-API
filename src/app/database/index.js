efdce32ce1f6d74d2405dc61c97f62206c7a6e29
herbert henrique <bert@pop-os.localdomain>
2021-10-26 06:36:46 -0300

    debugando

--- src/app/database/index.js
+++ src/app/database/index.js
@@ -1,31 +1,19 @@
-// const { Client } = require('pg');
+const { Client } = require('pg');
 
-// const client = new Client({
-//   host: process.env.PGPHOST,
-//   port: process.env.PGPORT,
-//   user: process.env.PGUSER,
-//   password: process.env.PGPASSWORD,
-//   database: process.env.PGDATABASE
-// });
-
-// client.connect();
-
-// exports.query = async (query, values) => {
-//   const { rows } = await client.query(query, values);
-//   return rows;
-// }
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+const client = new Client({
+  host: process.env.PGPHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-pool.on('connect', () => {
-  console.log('Base de Dados conectado com sucesso!');
-});
-
+client.connect();
 
 exports.query = async (query, values) => {
-  const { rows } = await pool.query(query, values);
+  const { rows } = await client.query(query, values);
   return rows;
 }
