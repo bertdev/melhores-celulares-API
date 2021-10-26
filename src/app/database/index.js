caabe6430e0aa12214b35a0a3d8bfd6503a69e7c
herbert henrique <bert@pop-os.localdomain>
2021-10-25 23:26:22 -0300

    debugando

--- src/app/database/index.js
+++ src/app/database/index.js
@@ -1,16 +1,31 @@
-const { Client } = require('pg');
+// const { Client } = require('pg');
 
-const client = new Client({
-  host: process.env.PGPHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE
+// const client = new Client({
+//   host: process.env.PGPHOST,
+//   port: process.env.PGPORT,
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   database: process.env.PGDATABASE
+// });
+
+// client.connect();
+
+// exports.query = async (query, values) => {
+//   const { rows } = await client.query(query, values);
+//   return rows;
+// }
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
 });
 
-client.connect();
+pool.on('connect', () => {
+  console.log('Base de Dados conectado com sucesso!');
+});
+
 
 exports.query = async (query, values) => {
-  const { rows } = await client.query(query, values);
+  const { rows } = await pool.query(query, values);
   return rows;
 }
