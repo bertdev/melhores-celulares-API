fb4f3fc93f6d236b0c70e0ff350755760c5c6850
herbert henrique <bert@pop-os.localdomain>
2021-10-26 12:17:44 -0300

    debugando

--- src/app/database/index.js
+++ src/app/database/index.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  host: process.env.PGPHOST,
+  host: process.env.PGHOST,
   port: process.env.PGPORT,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
